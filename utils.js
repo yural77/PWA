90c30a9290d35f11d77db231196ad3540a9b2212
yural77 <0612612@mail.ru>
2025-02-09 21:17:51 +0300

    test

--- utils.js
+++ utils.js
@@ -189,6 +189,7 @@
               if(KeyExtensions.prf['results'] == undefined || KeyExtensions.prf['results']['first'] == undefined) { PrintError("PRF is not supported on this device"); return {} }
               const data = arrayBufferToBase64(KeyExtensions.prf['results']['first']);
               PrintInfo('HMAC: ' + data, 'lightblue');
+              PrintInfo("In: " + arrayBufferToBase64(new Uint8Array([0,1,2,3,4,5,6,7,8,9, 0,1,2,3,4,5,6,7,8,9, 0,1,2,3,4,5,6,7,8,9, 1,2 ])));
               if(data === '') { PrintError("PRF is not supported on this device"); } 
             }
               
